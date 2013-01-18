c448899f3c370e513c7541250de03785543f9f6e
laurentmor <laurent.morissette@gmail.com>
2013-01-18 08:08:46 -0500

    Mise en place de la structure du projet

---
+++ GestionReclamation/src/main/java/ca/uqam/inf2015/equipe2/gestionreclamation/App.java
@@ -0,0 +1,13 @@
+package ca.uqam.inf2015.equipe2.gestionreclamation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
